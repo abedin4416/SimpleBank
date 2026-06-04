49e6d602e928e6bf797a51bdad9143aadf4f173f
Md. Amir Abedin <abedin4416@gmail.com>
2026-06-04 20:10:56 +0600

    account delete option added

--- bank.cpp
+++ bank.cpp
@@ -10,9 +10,9 @@ string input;
 void options(const vector<string>& options);
 bool load();
 void save();
-bool exists();
-string get(string name);
-void setval(string name, string value);
+bool exists(string id);
+string get(string id, string name);
+void setval(string id, string name, string value);
 bool verified();
 void newPassword();
 void newAccount();
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]){
     if(argc < 2) return 0;
     load();
     ID = argv[1];
-    if(!exists()){
+    if(!exists(ID)){
         newAccount(); 
         return 0;
     }
@@ -29,13 +29,27 @@ int main(int argc, char *argv[]){
         cout << "Wrong password.";
         return 0;
     }
-    cout << "Balance: " << get("money") << "\n";
-    options({"Send", "Change Password"});
-    //input 1 send
-    if(input == "2"){
+    cout << "Balance: " << get(ID, "money") << "\n";
+    options({"Send", "Change Password", "Delete Account"});
+    
+    if(input == "1"){
+        //send money
+    }
+    else if(input == "2"){
         newPassword();
         save();
         cout << "Password changed.";
+    }
+    else if(input == "3"){
+        if(stoi(get(ID, "money")) == 0){
+            cout << "Type DELETE to confirm: ";
+            cin >> input;
+            if(input != "DELETE") return 0;
+            db.erase(ID);
+            save();
+            cout << "Account deleted.\n";
+        }
+        else cout << "Account is not empty!\n";
     }
     return 0;
 }
@@ -61,38 +75,37 @@ void save(){
     out << db.dump(4);
 }
 
-bool exists(){
-    return db.contains(ID);
+bool exists(string id){
+    return db.contains(id);
 }
 
-string get(string name){
-    if(!db.contains(ID)) return "";
-    if(!db[ID].contains(name)) return "";
-    return db[ID][name].get<string>();
+string get(string id, string name){
+    if(!db.contains(id)) return "";
+    if(!db[id].contains(name)) return "";
+    return db[id][name].get<string>();
 }
 
-void setval(string name, string value){
-    db[ID][name] = value;
+void setval(string id, string name, string value){
+    db[id][name] = value;
 }
 
 bool verified(){
     cout << "Enter password: ";
     cin >> input;
-    return get("key") == input;
+    return get(ID, "key") == input;
 }
 
 void newPassword(){
     cout << "Enter a new password: ";
     cin >> input;
-    setval("key", input);
+    setval(ID, "key", input);
 }
 void newAccount(){
     cout << "The Account doesn't exist\n";
-    cout << "1. Create Account" << "\n";
-    cin >> input;
+    options({"Create Account"});
     if(input != "1") return;
     newPassword();
-    setval("money", "0");
+    setval(ID, "money", "0");
     save();
     cout << "Account created successfully.";
 }
